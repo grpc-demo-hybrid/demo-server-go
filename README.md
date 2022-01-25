87b91bd5cf561b1041dac25908318db28a1ce729
andy <dlut_lfm@aliyun.com>
2022-01-25 15:35:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# GRPC demo
+
+包含3个工程：
+
+demo-proto： 定义proto的工程
+
+demo-server-go: go语言实现的grpc server工程
+
+demo-client-java: java实现的grpc client工程
---
+++ Server.go
@@ -0,0 +1,47 @@
+//go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"context"
+
+	pb "github.com/gonzalez-lee/mygo/helloworld"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	fmt.Printf("parameters: name -> %s, age -> %d\n", in.Name, in.Age)
+	return &pb.HelloReply{Message: "Hello Again " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
